c0b174cbc513837f5380bb477a5ab63f026a5c24
KREMER Timothée <timothee.kremer@epitech.eu>
2025-11-10 15:54:35 +0100

    reverted to old version

--- project/src/conf.lua
+++ project/src/conf.lua
@@ -1,8 +1,15 @@
 -- conf.lua
-local game = require("data")
-local love = require("love")
+local dataModule = require("data")
+local data = dataModule.data or {}
 
+--[[
+==== To be executed once at program's execution
+==== Configures window
+]]--
 function love.conf(t)
-    t.window.width = game.game.WIDTH
-    t.window.height = game.game.HEIGHT
+    t.window.width = data.game.WIDTH
+    t.window.height = data.game.HEIGHT
+    t.window.title = ("Lua RPG")
+    t.window.resizable = false
+    t.window.vsync = true
 end
--- project/src/data.lua
+++ project/src/data.lua
@@ -1,19 +1,74 @@
 -- game.lua
-local data = {
+local json = require("libs.dkjson")
+local io = require("io")
+
+local module = {}
+
+module.data = {
+    --[[
+    ==== Contains all the game's data to run the game
+    ]]--
     game = {
-        WIDTH = 1920,
-        HEIGHT = 1080,
+        WIDTH = 800,
+        HEIGHT = 600,
     },
+    --[[
+    ==== Camera information
+    ]]--
     camera = {
         mode = 1;
         x = 0;
         y = 0;
     },
+    --[[
+    ==== Contains player's data
+    ]]--
     player = {
+        --[[
+        ==== 1 : down
+        ==== 2: left
+        ==== 3: right
+        ==== 4: top
+        ]]--
+        direction = 1,
         x = 0,
         y = 0,
-        speed = 200,
-    }
+        speed = 150,
+        inventory = {
+            money = 0;
+        }
+    },
 }
 
-return data
+function module.loadData()
+    local file = io.open("data.json", "r")
+    local data = nil
+
+    if not file then
+        module.saveData()
+        return module.loadData()
+    end
+    data = file:read("*a")
+    file:close()
+    if not data or data == "" then
+        error("Empty or corrupted data.")
+    end
+    module.data = json.decode(data)
+    return true
+end
+
+function module.saveData()
+    local file, err = io.open("data.json", "w")
+    local encodedData = nil
+
+    if not file then
+        error("Couldn't save save file " .. tostring(err))
+    end
+    encodedData = json.encode(module.data, { indent = true })
+    file:write(encodedData)
+    file:close()
+end
+
+module.loadData()
+
+return module
